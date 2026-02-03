3733594a346aed3f2de175c7a4b8bbfd3d57632b
Никита <fatomartyr@gmail.com>
2026-02-03 10:29:15 +0300

    Initial commit

---
+++ src/test/java/ru/neoflex/salarycalculator/SalaryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.salarycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
